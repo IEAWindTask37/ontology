5e0fd6a379a748fe1e84b9080856df8d14b48a50
Bortolotti <pbortolo@nrel.gov>
2018-11-29 13:17:18 -0700

    2d_fem added

--- turbine/pibo/validation.py
+++ turbine/pibo/validation.py
@@ -12,3 +12,9 @@ with open('IEAturbine_schema.yaml', 'r') as myfile:
   schema = myfile.read()
 
 validate(yaml.load(inputs), yaml.load(schema))
+
+
+wt_data= yaml.load(inputs)
+
+print(wt_data['components']['blade']['bem_aero']['coordinates']['y']['grid'])
+print(wt_data['components']['blade']['2d_fem']['sections'][0])

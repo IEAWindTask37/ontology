2039ae51af73498e4de5fb81c598cb596f74b0bc
mmpe <mmpe@dtu.dk>
2021-02-03 09:09:07 +0100

    add shear and ref height + update and add test of IEA37 case study AEP calculation

--- plant/examples/pywake_utils.py
+++ plant/examples/pywake_utils.py
@@ -7,37 +7,6 @@ from py_wake.wind_turbines import OneTypeWindTurbines, cube_power
 from py_wake.site.xrsite import XRSite
 
 
-# class PyWakeXRSiteLoader(Loader):
-#
-#     def include(self, node):
-#
-#         filename = os.path.join(self._root, self.construct_scalar(node))
-#         ext = os.path.splitext(filename)[1].lower()
-#         if ext in ['.yaml', '.yml']:
-#             with open(filename, 'r') as f:
-#                 return yaml.load(f, PyWakeXRSiteLoader)
-#         elif ext in ['.nc']:
-#
-#             def fmt(v):
-#                 if isinstance(v, dict):
-#                     return {k: fmt(v) for k, v in v.items() if fmt(v) != {}}
-#                 elif isinstance(v, tuple):
-#                     return list(v)
-#                 else:
-#                     return v
-#
-#             def ds2yml(ds):
-#                 ds = ds.rename(**{k: v for k, v in [('wd', 'directions'),
-#                                                     ('P', 'probability'),
-#                                                     ('WS', 'wind_speed')] if k in ds})
-#                 d = ds.to_dict()
-#                 return fmt({**d['coords'], **d['data_vars']})
-#             return ds2yml(xr.open_dataset(filename))
-#
-#
-# PyWakeXRSiteLoader.add_constructor('!include', PyWakeXRSiteLoader.include)
-
-
 def yml2Site(yml, interp_method='nearest'):
     resource = load_yaml(yml, XrResourceLoader)
     data = resource['wind_resource']
@@ -64,6 +33,7 @@ def xr2Site(ds, interp_method='nearest'):
 def yml2WindTurbines(yml):
     wt = load_yaml(yml)
     power = wt['power']
+
     if 'power_curve' in power:
         raise NotImplementedError()
     elif 'cp_curve' in power:
@@ -75,7 +45,10 @@ def yml2WindTurbines(yml):
                                 power_rated=power['rated_power'])
 
         def ct_func(ws):
-            return np.zeros_like(ws) + 8 / 9
+            return np.interp(ws,
+                             power['Ct_curve']['Ct_wind_speeds'],
+                             power['Ct_curve']['Ct_values'])
+
     return OneTypeWindTurbines(name=wt['name'], diameter=wt['rotor_diameter'], hub_height=wt['hub_height'],
                                power_func=power_func, ct_func=ct_func, power_unit='w')
 

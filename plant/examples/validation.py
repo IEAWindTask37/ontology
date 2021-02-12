0d8f6fb170c2d5000b7f79f056a63063e8aa74f0
bayc <christopher.j.bay@gmail.com>
2021-02-11 20:11:56 -0700

    adding all examples to validation; updating imports

--- plant/examples/validation.py
+++ plant/examples/validation.py
@@ -1,22 +1,65 @@
 import yaml
 import os
-from plant.examples.utils import validate_yaml, plant_schemas_path, XrResourceLoader
-from plant.examples.utils import examples_data_path
+from plant.examples.utils import plant_schemas_path, examples_data_path
+from plant.examples.utils.yml_utils import validate_yaml, XrResourceLoader
 
 
 if __name__ == '__main__':
 
-    validate_yaml(data_file=examples_data_path + 'wind_energy_system/IEA37_case_study12_wind_energy_system.yaml',
+    # ===================================================================================================================
+    # # validate IEA case studies 1 and 2
+    # ===================================================================================================================
+
+    validate_yaml(data_file=examples_data_path + 'wind_energy_system/IEA37_case_study_1_2_wind_energy_system.yaml',
                   schema_file=plant_schemas_path + 'wind_energy_system.yaml')
 
-    validate_yaml(data_file=examples_data_path + 'plant_energy_resource/IEA37_case_study_energy_resource.yaml',
+    validate_yaml(data_file=examples_data_path + 'plant_energy_resource/IEA37_case_study_1_2_energy_resource.yaml',
                   schema_file=plant_schemas_path + 'plant_energy_resource.yaml')
 
-    validate_yaml(data_file=examples_data_path + 'plant_energy_site/IEA37_case_study_energy_site.yaml',
+    validate_yaml(data_file=examples_data_path + 'plant_energy_site/IEA37_case_study_1_2_energy_site.yaml',
                   schema_file=plant_schemas_path + 'plant_energy_site.yaml')
 
-    validate_yaml(data_file=examples_data_path + 'plant_wind_farm/IEA37_case_study_wind_farm.yaml',
+    validate_yaml(data_file=examples_data_path + 'plant_wind_farm/IEA37_case_study_1_2_wind_farm.yaml',
                   schema_file=plant_schemas_path + 'plant_wind_farm.yaml')
+   
+    # ===================================================================================================================
+    # # validate IEA case study 3
+    # ===================================================================================================================
+
+    validate_yaml(data_file=examples_data_path + 'wind_energy_system/IEA37_case_study_3_wind_energy_system.yaml',
+                  schema_file=plant_schemas_path + 'wind_energy_system.yaml')
+
+    validate_yaml(data_file=examples_data_path + 'plant_energy_resource/IEA37_case_study_3_energy_resource.yaml',
+                  schema_file=plant_schemas_path + 'plant_energy_resource.yaml')
+
+    validate_yaml(data_file=examples_data_path + 'plant_energy_site/IEA37_case_study_3_energy_site.yaml',
+                  schema_file=plant_schemas_path + 'plant_energy_site.yaml')
+    
+    validate_yaml(data_file=examples_data_path + 'plant_wind_farm/IEA37_case_study_3_wind_farm.yaml',
+                  schema_file=plant_schemas_path + 'plant_wind_farm.yaml')
+
+    # ===================================================================================================================
+    # # validate IEA case study 4
+    # ===================================================================================================================
+
+    validate_yaml(data_file=examples_data_path + 'wind_energy_system/IEA37_case_study_4_wind_energy_system.yaml',
+                  schema_file=plant_schemas_path + 'wind_energy_system.yaml')
+
+    validate_yaml(data_file=examples_data_path + 'plant_energy_resource/IEA37_case_study_4_energy_resource.yaml',
+                  schema_file=plant_schemas_path + 'plant_energy_resource.yaml')
+
+    validate_yaml(data_file=examples_data_path + 'plant_energy_site/IEA37_case_study_4_energy_site.yaml',
+                  schema_file=plant_schemas_path + 'plant_energy_site.yaml')
+
+    validate_yaml(data_file=examples_data_path + 'plant_wind_farm/IEA37_case_study_4_wind_farm.yaml',
+                  schema_file=plant_schemas_path + 'plant_wind_farm.yaml')
+
+    # ===================================================================================================================
+    # # validate IEA turbines
+    # ===================================================================================================================
+
+    validate_yaml(data_file=examples_data_path + 'plant_energy_turbine/IEA37_3.35MW_turbine.yaml',
+                  schema_file=plant_schemas_path + 'plant_energy_turbine.yaml')
 
     validate_yaml(data_file=examples_data_path + 'plant_energy_turbine/IEA37_10MW_turbine.yaml',
                   schema_file=plant_schemas_path + 'plant_energy_turbine.yaml')
